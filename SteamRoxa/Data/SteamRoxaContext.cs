1d685f84d32e18e66ea12306acbe4b5616c99653
DevErick <djerickdev@gmail.com>
2025-02-05 16:05:04 -0300

    Add Controllers

--- SteamRoxa/Data/SteamRoxaContext.cs
+++ SteamRoxa/Data/SteamRoxaContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SteamRoxa.Models;
 
 namespace SteamRoxa.Data
 
@@ -10,10 +11,17 @@ namespace SteamRoxa.Data
         public SteamRoxaContext(DbContextOptions<SteamRoxaContext> options) : base(options)
         { }
 
+        //Definir as tabelas do banco de dados
+
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
         //sobreescrever o metodo OnModelCreating
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Jogo>().ToTable("Jogos");
+            modelBuilder.Entity<Categoria>().ToTable("Categorias");
         }
 
     }
